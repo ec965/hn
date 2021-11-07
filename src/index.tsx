cb14c8bf827f8fd708a1d4d07dc86a4cfd5f1cc3
ec965 <enoch965@gmail.com>
2021-11-07 01:04:49 -0700

    configure theming

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,5 @@ import { render } from "solid-js/web";
 import { App } from "./App";
 import "./styles/global.scss";
 
+document.documentElement.className = "dark";
 render(() => <App />, document.getElementById("root") as HTMLElement);
--- src/lib/Footer.tsx
+++ src/lib/Footer.tsx
@@ -1,11 +1,33 @@
+import { createSignal } from "solid-js";
+
 import style from "./Footer.module.scss";
 
 export function Footer() {
+  const [rootTheme, setRootTheme] = createSignal(
+    document.documentElement.className
+  );
+
+  const switchTheme = () => {
+    document.documentElement.className =
+      rootTheme() === "dark" ? "light" : "dark";
+    setRootTheme(document.documentElement.className);
+  };
+
+  const scrollToTop = () => {
+    document.documentElement.scrollTop = 0;
+  };
+
   return (
     <footer class={style.footer}>
       <a target="_blank" href="https://github.com/ec965/hn">
         source code
       </a>
+      <p class={style.clickable} onClick={switchTheme}>
+        {rootTheme() === "dark" ? "light mode" : "dark mode"}
+      </p>
+      <p class={style.clickable} onClick={scrollToTop}>
+        back to top
+      </p>
     </footer>
   );
 }
